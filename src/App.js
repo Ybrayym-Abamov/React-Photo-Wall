a27a0dbdfe35efd60bed4ecbe8dee9c122a315ce
Ybrayym <ybrayymjan90@gmail.com>
2019-10-30 14:50:03 -0400

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
-// This URL can be combined with an photo id to fetch an photo.
+// This URL can be combined with a photo id to fetch a photo.
 const PHOTO_URL = photoId => `https://picsum.photos/id/${photoId}/200/200`;
 // This URL can be used to get an array of objects that contain information
 // about various photos.
@@ -9,24 +9,31 @@ const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {};
-
+  state = {
+    photos: []
+  };
   // 2. Declare a life cycle method
   // This life cycle method should:
   //  - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
+  componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then(res => res.json())
+      .then(
+        result =>
+          this.setState({
+            photos: result
+          })
+      )
+  }
   render() {
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
+        {console.log(photos)}
         <header>
           <h1>Photo Wall</h1>
-          <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and complete
-            the instructions there.
-          </p>
         </header>
         <div className="collage">
           {/* We use map here because Array.prototype.map is an expression,
@@ -34,9 +41,9 @@ class App extends Component {
            */}
           {photos.map(photo => (
             <img
-              alt={/* 3. Fill me in with the photo's filename */ ""}
-              key={/* 4. Fill me in with the photo's id */ ""}
-              src={/* 5. Fill me in with the photo's URL */ ""}
+              alt={/* 3. Fill me in with the photo's filename */ photo.filename}
+              key={/* 4. Fill me in with the photo's id */ photo.id}
+              src={/* 5. Fill me in with the photo's URL */ PHOTO_URL(photo.id)}
             />
           ))}
         </div>
